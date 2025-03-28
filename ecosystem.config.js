b88e9c783d29a94281bbca22923ddc1a38d061e7
IronLu233 <ironlu233.dev@outlook.com>
2025-03-28 12:03:13 +0800

    fix: update deployment path for Bull Dashboard worker in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
     app: {
       user: "ubuntu",
       host: [],
-      ref: "origin/master",
+      ref: "origin/v2",
       repo: "git@github.com:IronLu233/trade-terminal-account-board.git",
       path: "/home/ubuntu/bull-dashboard",
       "post-deploy":
@@ -31,9 +31,9 @@ module.exports = {
     worker: {
       user: "ubuntu",
       host: ["3.115.99.150", "3.115.219.178", "52.197.123.66"],
-      ref: "origin/master",
+      ref: "origin/v2",
       repo: "git@github.com:IronLu233/trade-terminal-account-board.git",
-      path: "/home/ubuntu/bull-dashboard",
+      path: "/home/ubuntu/bull-dashboard-worker",
       "post-deploy":
         "bun install && pm2 startOrRestart ecosystem.config.js --only account-board-worker",
     },
